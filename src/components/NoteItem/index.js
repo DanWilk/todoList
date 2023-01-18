b5a923eb885b9d1cd94dc01827988d83156a3c6e
unknown <dcwilkinson@wilktech.com>
2023-01-17 22:12:57 -0500

    created and laid out Notes list

---
+++ src/components/NoteItem/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NoteItem = (key) => {
+    return (
+        <div className='NoteItmem' key={key}>
+            <div className='NoteTitleArea'>
+                <h3>Title 1</h3>
+                <p className='NoteDueDate'>1/26/2023</p>
+            </div>
+            <div className='NoteText'>
+                <p>Ea proident officia Lorem excepteur ullamco ad magna veniam tempor nulla. Voluptate cupidatat reprehenderit adipisicing consequat. Ex irure culpa deserunt nostrud aute veniam esse consectetur nostrud dolor nostrud. Eiusmod veniam minim dolor cupidatat sit enim cupidatat ipsum do irure mollit ullamco. Quis cupidatat elit officia Lorem fugiat occaecat incididunt consequat minim.</p>
+            </div>
+        </div>
+    )
+}
+
+export default NoteItem;
--- src/components/NoteListItem/index.js
+++ src/components/NoteListItem/index.js
@@ -1,11 +1,16 @@
 import React from "react";
+import NoteItem from "../NoteItem";
 
 const NoteListItem = () => {
-
+    let array = [1, 2, 3, 4, 5]
 
     return (
-        <div>
-            
-        </div>
+        <section className="itemListContainer">
+            {array.map((item, i) => {
+                return <NoteItem key={i}/>
+            })}
+        </section>
     )
 }
+
+export default NoteListItem;
