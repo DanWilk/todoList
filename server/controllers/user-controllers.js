d5d71ed62fb47b5726c168c019b4e14ad15de9a4
unknown <dcwilkinson@wilktech.com>
2023-02-16 21:30:46 -0500

    prevented password and __v property from being returned on user lookup

--- server/controllers/user-controllers.js
+++ server/controllers/user-controllers.js
@@ -6,9 +6,9 @@ const userController = {
         User.findOne({_id: params.id})
         .populate({
             path: "todos",
-            // select: "-__v"
+            select: "-__v"
         })
-        // .select(-_v)
+        .select("-__v -password")
         .then(dbUserData => {
             if(!dbUserData) {
                 res.status(404).json({message: "No User found with this id!"});
