85d9871ca1668bb2e20eaaa3eacc34f5da37192c
unknown <dcwilkinson@wilktech.com>
2023-02-19 22:27:42 -0500

    connected front to back getting cors error

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
+import Auth from "../../utils/auth";
 
 const Login = () => {
+
+    const [userData, setUserData] = useState({usename: "", email: ""});
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setUserData({
+            ...userData,
+            [name]: value,
+        });
+    }
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        
+        try {
+            await fetch("http://localhost3001.com/api/user", {
+                method: "POST",
+                body: JSON.stringify(userData),
+                
+            })
+            .then(dbUserData => {
+                console.log(dbUserData);
+                Auth.login(dbUserData);
+            })
+        } catch(e) {
+            console.error(e);
+        }
+    }
     return (
     <main>
-        <form class="loginForm">
-            <label for="email">Email: </label>
-            <input type="email" name="email" id="email"></input>
-            <label for="password">Password: </label>
-            <input type="password" name="password" id="password"></input>
+        <form className="loginForm" onSubmit={handleFormSubmit}>
+            <label>Email: 
+            <input type="email" name="email" id="email" onChange={handleChange}></input>
+            </label>
+            <label>Password: 
+            <input type="password" name="password" id="password" onChange={handleChange}></input>
+            </label>
+            <button type="submit">Login</button>
         </form>
+
     </main>
     )
 }
