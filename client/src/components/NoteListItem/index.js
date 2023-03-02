120c1585817dd7f0a8bf150abf2399e6c7c6285d
unknown <dcwilkinson@wilktech.com>
2023-03-01 22:50:47 -0500

    got front end to query users from frontend

--- client/src/components/NoteListItem/index.js
+++ client/src/components/NoteListItem/index.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+// import {useParams} from "react-router-dom";
 import NoteItem from "../NoteItem";
 
 const NoteListItem = () => {
+
+    const [todoList, setTodoList] = useState([]);
+
+    const loadTodos = async () => {
+
+        console.log(todoList)
+        // if(todoList.length <= 0) {
+        //     return;
+        // }
+
+        // let {id} = useParams();
+
+            await fetch(`http://localhost:3001/api/user${window.location.pathname}`)
+            .then(response => response.json())
+            .then(dbUserData => {
+                console.log(dbUserData);
+                // setTodoList(dbUserData);
+                // console.log(todoList);
+            })
+            .catch(err => console.log(err))
+    }
+
+    loadTodos();
     let array = [1, 2, 3, 4, 5]
 
     return (
