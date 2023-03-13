8a68d39330893d66e44a00a0c39c6270d3ffb315
unknown <dcwilkinson@wilktech.com>
2023-03-13 19:13:58 -0400

    added user creation to frontend

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -4,6 +4,7 @@ import Auth from "../../utils/auth";
 const Login = () => {
 
     const [userData, setUserData] = useState({email: "", password: ""});
+    const [signupData, setSignupData] = useState({username: "", email: "", password: ""})
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -15,6 +16,16 @@ const Login = () => {
         });
     }
 
+    const handleSignupChange = (event) => {
+        const {name, value} = event.target;
+        console.log(signupData)
+
+        setSignupData({
+            ...signupData,
+            [name]: value
+        });
+    }
+
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         
@@ -23,6 +34,28 @@ const Login = () => {
                 method: "POST",
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(userData),
+                
+            })
+            .then( response => response.json())
+            .then( (userData) => {
+                console.log(userData);
+                Auth.login(userData.token);
+                if(Auth.loggedIn()) {
+                    window.location.assign(`/${userData.user._id}`)
+                }
+            })
+        } catch(e) {
+            console.error(e);
+        }
+    }
+    const handleSignupSubmit = async (event) => {
+        event.preventDefault();
+        
+        try {
+            await fetch("http://localhost:3001/api/user", {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(signupData),
                 
             })
             .then( response => response.json())
@@ -49,21 +82,24 @@ const Login = () => {
             <input className="passwordInput" type="password" name="password" id="password" onChange={handleChange}></input>
             </label>
             <div className="buttonContainer">
-            <button type="submit">Login</button>
+                <button type="submit">Login</button>
             </div>
         </form>
         </div>
         <div className="FormContainer">
-            <form className="signupForm">
+            <form className="signupForm" onSubmit={handleSignupSubmit}>
                 <label className="usernameLabel">Username:
-                    <input className="usernameInput" type="text" name="username" id="username" onChange={handleChange}></input>
+                    <input className="usernameInput" type="text" name="username" id="signupUsername" onChange={handleSignupChange}></input>
                 </label>
                 <label className="emailLabel">Email:
-                    <input className="emailInput" type="email" name="email" id="email"></input>
+                    <input className="emailInput" type="email" name="email" id="signupEmail" onChange={handleSignupChange}></input>
                 </label>
                 <label className="passwordLabel">Password: 
-                    <input className="passwordInput" type="password" name="password" id="password"></input>
+                    <input className="passwordInput" type="password" name="password" id="signupPassword" onChange={handleSignupChange}></input>
                 </label>
+                <div className="buttonContainer">
+                    <button type="submit">Signup</button>
+                </div>
             </form>
         </div>
     </main>
